a66b0831f5c714f7506e89ab46c7d8131a2b9ba2
Christina <christinalorenzomontes@gmail.com>
2023-09-18 22:10:34 +0200

    add some functionalities

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function createBoard() {
     square.classList.add('square')
     square.innerHTML = startPiece
     square.firstChild?.setAttribute('draggable', true)
-    square.classList.add('square-id', i)
+    square.setAttribute('square-id', i)
     const row = Math.floor((63 - i )/ 8) + 1
     if( row % 2 === 0) {
       square.classList.add(i % 2 === 0 ? 'beige' : 'brown')
@@ -43,11 +43,26 @@ const allSquares = document.querySelectorAll("#gameboard .square")
 
 allSquares.forEach(square => {
   square.addEventListener('dragstart', dragStart)
+  square.addEventListener('dragover', dragOver)
+  square.addEventListener('drop', dragDrop)
 })
 
 let startPositionId
+let draggedElement
 
 function dragStart (e) {
   startPositionId = e.target.parentNode.getAttribute('square-id')
-  console.log(startPositionId)
+  draggedElement = e.target
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop(e) {
+  e.stopPropagation();
+  
+  e.target.parentNode.append(draggedElement)
+  e.target.remove()
+  e.target.append(draggedElement)
 }
